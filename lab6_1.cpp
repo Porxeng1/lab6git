56aa98466a28f3ae4716fa1bd2e2a2072a537354
Phattadon <phattadon1st@gmail.com>
2024-12-03 01:25:36 +0700

    Filled out lab6_1and_2

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int even = 0, odd = 0 ;
+    int x;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while(x!=0){
+        if(x%2==0){
+            even++;
+        }else{
+            odd++;
+        }    
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
